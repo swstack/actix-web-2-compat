69c270f60d33f2ae32c586f16604be82ab7e52e9
Stephen Stack <sfstack7500@gmail.com>
2020-08-19 14:54:42 -0500

    App: Update example

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,30 +1,22 @@
 use crate::fut1_client;
 use crate::fut3_client;
 use actix_web2::http::StatusCode;
-use actix_web2::{web, HttpRequest, HttpResponse};
+use actix_web2::HttpResponse;
 use futures3::compat::Future01CompatExt;
 
 pub struct AppState {
     pub app_name: String,
 }
 
-pub async fn index(data: web::Data<AppState>, req: HttpRequest) -> HttpResponse {
-    println!("Hello {}!", &data.app_name);
-    println!("path: {:?}", req.path());
-    println!(
-        "header: {:?}",
-        req.headers().get("X-Platform-Api-Token").unwrap()
-    );
-    println!("query: {:?}", req.query_string());
-
+pub async fn index() -> HttpResponse {
     let fut3_http_req = fut3_client::make_http_request().await;
     println!("{:?}", fut3_http_req.unwrap());
 
     let fut3_sleep = fut3_client::sleep().await;
     println!("{:?}", fut3_sleep.unwrap());
-    //
-    // let fut1_http_req = fut1_client::make_http_request().compat().await;
-    // println!("{:?}", fut1_http_req.unwrap());
+
+    let fut1_http_req = fut1_client::make_http_request().compat().await;
+    println!("{:?}", fut1_http_req.unwrap());
 
     let fut1_sleep = fut1_client::sleep().compat().await;
     println!("{:?}", fut1_sleep.unwrap());
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
     let local_tasks = tokio2::task::LocalSet::new();
     let sys = actix_rt::System::run_in_tokio("main", &local_tasks);
     local_tasks.spawn_local(sys);
-    runtime.block_on_std(local_tasks.run_until(run_server()));
+    runtime
+        .block_on_std(local_tasks.run_until(run_server()))
+        .unwrap();
 }
 
 async fn run_server() -> std::io::Result<()> {
@@ -18,7 +20,7 @@ async fn run_server() -> std::io::Result<()> {
             .data(endpoint::AppState {
                 app_name: String::from("Actix-web"),
             })
-            .route("/execute", web::post().to(endpoint::index))
+            .route("/", web::post().to(endpoint::index))
     })
     .bind("127.0.0.1:8242")?
     .run()
